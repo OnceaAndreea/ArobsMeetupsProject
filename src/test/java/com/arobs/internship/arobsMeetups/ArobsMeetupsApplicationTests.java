bfe8de96868c41941cd6b9bf5924476dadaacf77
Oncea Andreea <andreeaoncea55@gmail.com>
2019-12-11 14:17:47 +0200

    First commit

---
+++ src/test/java/com/arobs/internship/arobsMeetups/ArobsMeetupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arobs.internship.arobsMeetups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArobsMeetupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
